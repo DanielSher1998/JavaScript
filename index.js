4a0da966e95226d69c3383d484053868926f6207
DanielSher1998 <danielsher02121998@gmail.com>
2022-02-18 13:39:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -36,3 +36,19 @@ let selection = 'name';
 person[selection] = 'Mary';
 
 console.log(person.name);
+
+let selectedColor = ['red', 'blue'];
+selectedColor[2] = 1;
+console.log(selectedColor.length);
+
+// Performing a task
+function greet(name, lastName) {
+  console.log('Hello ' + name + ' ' + lastName);
+}
+
+// Calculating a value
+function square(number) {
+  return number * number;
+}
+
+console.log(square(2));
