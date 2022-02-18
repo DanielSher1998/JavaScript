afe5f07fe1e672d3d1d7aeaac102d5af05acd025
DanielSher1998 <danielsher02121998@gmail.com>
2022-02-18 13:06:25 +0200

    Initial Files

---
+++ index.js
@@ -0,0 +1,24 @@
+// This is my first JavaScript code!
+console.log('Hello World');
+
+let name = 'Mosh';
+console.log(name);
+
+// Cannot be a reserved keyword
+// Should be meaningful
+// Cannot start with a number (1name)
+// Cannot contain a space or hyphen (-)
+// Are case-sensitive
+
+let firstName = 'Mosh';
+let lastName = 'Hamedani';
+
+const interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate);
+
+let name = 'Mosh'; // String Literal
+let age = 30; // Number Literal
+let isApproved = false; // Boolean Literal
+let firstName = undefined;
+let selectedColor = null;
