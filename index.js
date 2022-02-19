0f416b25393a47d17f19a74cdab6d3325761bbfe
DanielSher1998 <danielsher02121998@gmail.com>
2022-02-19 11:58:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,53 +1,71 @@
-    // This is my first JavaScript code!
-    console.log('Hello World');
+const friend1 = 'Michael';
+const friend2 = 'Steven';
+const friend3 = 'Peter';
 
-    // Performing a task
-    function greet(name, lastName) {
-      console.log('Hello ' + name + ' ' + lastName);
-    }
+const friends = ['Michael', 'Steven', 'Peter'];
+console.log(friends);
 
-    greet('John', 'Smith');
-    greet('Mary');
+const years = new Array(1991, 1984, 2008, 2020);
 
-    // Calculating a value
-    function square(number) {
-      return number * number;
-    }
+console.log(friends[0]);
+console.log(friends[2]);
 
-    console.log(square(2));
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
 
-    let selectedColors = ['red', 'blue'];
-    selectedColors[2] = 'green';
-    console.log(selectedColors.length);
-    
-    let name = 'Mosh'; // String Literal
-    let age = 30; // Number Literal
-    let isApproved = false; // Boolean Literal
-    let firstName = undefined;
-    let person = {
-      name: 'Mosh',
-      age: 30
-    };
+friends[2] = 'Jay';
+console.log(friends);
+// friends = ['Bob', 'Alice']
 
-    // Dot Notation
-    person.name = 'John';
+const firstName = 'Jonas';
+const jonas = [firstName, 'Schmedtmann', 2037 - 1991, 'teacher', friends];
+console.log(jonas);
+console.log(jonas.length);
 
-    // Bracket Notation
-    let selection = 'name';
-    person[selection] = 'Mary';
+// Exercise
+const calcAge = function (birthYeah) {
+  return 2037 - birthYeah;
+}
 
-    console.log(person.name);
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+console.log(age1, age2, age3);
 
-    let interestRate = 0.3;
-    interestRate = 1;
-    console.log(interestRate);
+const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
+console.log(ages);
 
-    console.log(name);
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
 
-    // Cannot be a reserved keyword
-    // Should be meaningful
-    // Cannot start with a number (1name)
-    // Cannot contain a space or hyphen (-)
-    // Are case-sensitive
+for (let i = 0; i < bills.length; i++) {
+  const tip = calcTip(bills[i]);
+  tips.push(tip);
+  totals.push(tip + bills[i]);
+}
+console.log(bills, tips, totals);
 
-    let lastName = 'Hamedani';
+const calcAverage =  function (arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    // sum = sum + arr[i];
+    sum += arr[i];
+  }
+  return sum / arr.length;
+}
+console.log(calcAverage([2, 3, 7]));
+console.log(calcAverage(totals));
+console.log(calcAverage(tips));
+
+'use strict';
+
+// Selecting elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+
+// Starting conditions
